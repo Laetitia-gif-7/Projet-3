40c56c4cef1d53d245f146fea915318a9649ae13
AIT-OUARAB Laetitia <laetitia.aitouarab@gmail.com>
2021-08-02 19:32:53 +0200

    Creation de la table troupeau + modification de Utilisateur : changement de List à Set

---
+++ TontapatV2/Entity/src/main/java/fr/eql/ai109/projet3/entity/Troupeau.java
@@ -0,0 +1,134 @@
+package fr.eql.ai109.projet3.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="troupeau")
+public class Troupeau implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_troupeau", unique=true, nullable=false)
+	private int idTroupeau;
+	
+	@Column(name="nom_troupeau", length=254)
+	private String nomTroupeau;
+	
+	@Column(length=254)
+	private String description;
+	
+	@Column(length=254)
+	private String photo;
+	
+	@Column(name="date_creation")
+	private Date dateCreation;
+	
+	@Column(name="date_retrait")
+	private Date dateRetrait;
+	
+	@Column(name="frequence_visite")
+	private int frequenceVisite;
+	
+	@Column(name="cout_par_animal", nullable=true)
+	private float coutParAnimal;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="id_utilisateur", nullable=false)
+	private Utilisateur utilisateur;
+	
+	/*
+	DEMANDER A MICHAEL DE MODIFIER LA BDD
+	@ManyToOne(fetch  = FetchType.LAZY)
+	@JoinColumn(name="id_motif_retrait_troupeau")
+	private MotifRetraitTroupeau motifRetraitTroupeau;
+	*/
+
+	public int getIdTroupeau() {
+		return idTroupeau;
+	}
+
+	public void setIdTroupeau(int idTroupeau) {
+		this.idTroupeau = idTroupeau;
+	}
+
+	public String getNomTroupeau() {
+		return nomTroupeau;
+	}
+
+	public void setNomTroupeau(String nomTroupeau) {
+		this.nomTroupeau = nomTroupeau;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public Date getDateRetrait() {
+		return dateRetrait;
+	}
+
+	public void setDateRetrait(Date dateRetrait) {
+		this.dateRetrait = dateRetrait;
+	}
+
+	public int getFrequenceVisite() {
+		return frequenceVisite;
+	}
+
+	public void setFrequenceVisite(int frequenceVisite) {
+		this.frequenceVisite = frequenceVisite;
+	}
+
+	public float getCoutParAnimal() {
+		return coutParAnimal;
+	}
+
+	public void setCoutParAnimal(float coutParAnimal) {
+		this.coutParAnimal = coutParAnimal;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	
+	
+
+}
--- TontapatV2/Entity/src/main/java/fr/eql/ai109/projet3/entity/Utilisateur.java
+++ TontapatV2/Entity/src/main/java/fr/eql/ai109/projet3/entity/Utilisateur.java
@@ -3,6 +3,7 @@ package fr.eql.ai109.projet3.entity;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -57,9 +58,12 @@ public class Utilisateur implements Serializable  {
 	@JoinColumn(name="insee_id", nullable=false)
 	private VilleCpRef villeCp;
 	
-	// TODO: check if necessary
+	 //TODO: check if necessary
 	@OneToMany(mappedBy="utilisateur", fetch = FetchType.EAGER)
-	private List<Terrain> terrains;
+	private Set<Terrain> terrains;
+
+	@OneToMany(mappedBy="utilisateur", fetch = FetchType.EAGER)
+	private Set<Troupeau> troupeaux;
 	
 	// Try with use of default constructor
 	
@@ -144,11 +148,17 @@ public class Utilisateur implements Serializable  {
 		this.villeCp = villeCp;
 	}
 	
-	public List<Terrain> getTerrains() {
+	public Set<Terrain> getTerrains() {
 		return terrains;
 	}
-	public void setTerrains(List<Terrain> terrains) {
+	public void setTerrains(Set<Terrain> terrains) {
 		this.terrains = terrains;
+	}
+	public Set<Troupeau> getTroupeaux() {
+		return troupeaux;
+	}
+	public void setTroupeaux(Set<Troupeau> troupeaux) {
+		this.troupeaux = troupeaux;
 	}
 	
 	
