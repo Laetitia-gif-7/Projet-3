24c2ee74d8a4d9a8b74315dfc48202cd92e05bb5
maiki93 <maiki@eql109>
2021-08-09 08:54:33 +0200

    first working version with JPA

--- TontapatV2/Dao/src/main/java/fr/eql/ai109/projet3/dao/CherchePrestations.java
+++ TontapatV2/Dao/src/main/java/fr/eql/ai109/projet3/dao/CherchePrestations.java
@@ -44,15 +44,45 @@ public class CherchePrestations implements CherchePrestationIDao {
 	public List<Troupeau> chercheTroupeauxCompatibles(Terrain terrain) {
 		List<Troupeau> correspondances = new ArrayList<>();
 		
+		//TypedQuery<Troupeau>
+		Query query = entityManager.createQuery(
+				"SELECT tr "
+				+ "FROM PeriodeDisponibilite pd, PeriodeDisponibilite pd2 "
+				+ "JOIN pd2.troupeau tr "
+				+ "WHERE " // pd2.troupeau is not null "
+				+ "pd.terrain = :paramTerrain "
+				+ "AND ( not ( pd.finPeriode < pd2.debutPeriode OR pd.debutPeriode > pd2.finPeriode ) ) "
+		);
+				
+//				+ "WHERE pd.finPeriode < :paramDate " working
+//				+ "JOIN Troupeau tr "
+//				+ "JOIN PeriodeDisponibilite pd2 ON tr.periodeDisponibilites = pd2 "
+//				+ "WHERE not ( pd.fin_periode < pd2.debut_periode OR pd.debut_periode > pd2.fin_periode ) "
+//				+ " AND t = :paramTerrain "
+				//-- Range periode must overlap <=> not not overlapping , use of auto jointure
+				//JOIN  periodedisponibilite as pd2 ON tr.troupeau_id = pd2.troupeau_id
+				//--  ON not ( pd.fin_periode < pd2.debut_periode OR pd.debut_periode > pd2.fin_periode )
+				//,Troupeau.class);
+		
+		query.setParameter("paramTerrain", terrain);
+		// query.setParameter("paramDate", new Date(2025,01,01));
+		correspondances = query.getResultList();
+		
+		return correspondances;
+	}
+	
+	
+	public List<Troupeau> chercheTroupeauxCompatibles3(Terrain terrain) {
+		List<Troupeau> correspondances = new ArrayList<>();
+		
 		TypedQuery<Troupeau> query = entityManager.createQuery(
-				  "SELECT tr "
-				+ "FROM Terrain t "
-				+ "JOIN t.periodeDisponibilites pd "
-				+ "JOIN Troupeau tr "
-				+ "JOIN tr.periodeDisponibilites pd2 "
-				+ "WHERE ( not ( pd.finPeriode < pd2.debutPeriode "
-				+ " OR pd.debutPeriode > pd2.finPeriode ) ) "
-				+ " AND t = :paramTerrain "
+				  "SELECT pd.troupeau "
+				+ "FROM PeriodeDisponibilite pd "
+				+ "JOIN PeriodeDisponibilite pd2 "
+//				+ "JOIN Troupeau tr "
+//				+ "JOIN tr.periodeDisponibilites pd2 "
+				+ "WHERE ( not ( pd.finPeriode < pd2.debutPeriode OR pd.debutPeriode > pd2.finPeriode ) ) "
+//				+ "AND t = :paramTerrain "
 				
 //				+ "WHERE pd.finPeriode < :paramDate " working
 //				+ "JOIN Troupeau tr "
@@ -64,7 +94,7 @@ public class CherchePrestations implements CherchePrestationIDao {
 				//--  ON not ( pd.fin_periode < pd2.debut_periode OR pd.debut_periode > pd2.fin_periode )
 				,Troupeau.class);
 		
-		query.setParameter("paramTerrain", terrain);
+		//query.setParameter("paramTerrain", terrain);
 		// query.setParameter("paramDate", new Date(2025,01,01));
 		correspondances = query.getResultList();
 		
